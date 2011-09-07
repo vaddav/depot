6543b0b51ec22cf14ab29f6d099b70ddfc83ad62
vaddav <vityaz00@gmail.com>
2011-09-07 10:22:22 +0300

    Playtime after TASK B (Change the error message associated with one of your validations)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-  validates :title, :uniqueness => true, :length => { :minimum => 10 }
+  validates_presence_of :title, :description, :image_url, :message => 'Empty!!!!'
+  #validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates_numericality_of   :price, :greater_than_or_equal_to => 0.01 , :message => 'Not number!!!'
+  validates :title, :length => {:minimum => 10, :too_short => 'Short!!!'}
+  validates_uniqueness_of :title, :message => 'Must be unique!!!'
   validates :image_url, :format => {
   :with => %r{\.(gif|jpg|png)$}i,
   :message => 'must be a URL for GIF, JPG or PNG image.'
