1b73048d41d4d81338cd1427a229ea66d8f15031
vaddav <vityaz00@gmail.com>
2011-09-13 09:06:32 +0300

    Playtime after Task D: Add a new variable to the session to record how many times the user has accessed the store controller’s index action.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @count = session_count
   end
 
+  def session_count
+     if session[:counter].nil?
+      session[:counter] = 0
+     end
+      session[:counter] += 1
+  end
 end
