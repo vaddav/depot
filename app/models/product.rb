e7c104454462b5a8cbb70ad47774cc2b26bcd608
vaddav <vityaz00@gmail.com>
2011-09-15 10:55:05 +0300

    playtime after Task E (Check products and line items for other places where a user-friendly error message would be in order.)

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,13 @@ class Product < ActiveRecord::Base
   validates_numericality_of   :price, :greater_than_or_equal_to => 0.01 , :message => 'Not number!!!'
   validates :title, :length => {:minimum => 10, :too_short => 'Short!!!'}
   validates_uniqueness_of :title, :message => 'Must be unique!!!'
-  validates :image_url, :format => {
+  validates :image_url,:uniqueness => true ,:format => {
   :with => %r{\.(gif|jpg|png)$}i,
   :message => 'must be a URL for GIF, JPG or PNG image.'
 }
   default_scope :order => 'title'
+
+  validates :price, :numericality => {:less_than_or_equal_to => 100}
 
   has_many :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
